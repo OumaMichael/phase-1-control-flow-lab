061bb12a88e26774d852d8f2ab6f16ed7a1afe84
Michael onyango ouma <oumamichael108@gmail.com>
2025-03-05 19:24:17 +0300

    Complete control flow lab: implemented scuberGreetingForFeet, ternaryCheckCity, and switchOnCharmFromTip

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+/*
+scuberGreetingForFeet(distance)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+When called with a distance of 199, it should return:
+"This one is on me!"
+Additional tests will check for other distance ranges.
+A common specification (from similar labs) is:
+If distance ≤ 400: return "This one is on me!"
+If distance > 400 and ≤ 2000: return "That will be twenty bucks."
+If distance > 2000 and ≤ 2500: return "I will gladly take your thirty bucks."
+If distance > 2500: return "No can do."
+ternaryCheckCity(city)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+Use a ternary operator to return a response based on the city.
+If the city is "NYC", return "Ok, sounds good."
+Otherwise, return "No go."
+switchOnCharmFromTip(tip)
+
+Use a switch statement to return a response based on the tip.
+If tip is "generous", return "Thank you so much."
+If tip is "not as generous", return "Thank you."
+For any other tip value, return "Bye."
+*/
+function scuberGreetingForFeet(distance){
+if (distance <= 400){
+  return "This one is on me!";
+} else if (distance > 400 && distance <= 2000){
+  return "That will be twenty bucks.";
+} else if (distance > 2000 && distance <= 2500){
+  return "I will gladly take your thirty bucks.";
 }
+else {  return "No can do.";
+  }
+};   
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";   
+};
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  } 
+};
+
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharm
+};
